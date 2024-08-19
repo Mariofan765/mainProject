c7bfc8c44ccadfb171167f1499040a94898149de
Mariofan765 <76411784+Mariofan765@users.noreply.github.com>
2024-08-19 08:46:45 +0000

    0-task

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Сумма элементов массива: Напишите программу, которая вычисляет сумму всех элементов в заданном массиве."""
+
+def sum_arr(*a):
+    total = sum(a)
+    print(total)
+    return total
+
+
+input_string = input("Введите числа через пробел: ")
+numbers = list(map(int, input_string.split()))
+
+
+sum_arr(*numbers)
