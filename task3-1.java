140ffcf171a0f855dd4d62dc0c532dafc7caaf12
Mariofan765 <76411784+Mariofan765@users.noreply.github.com>
2024-09-16 22:38:11 +0300

    Update task3-1.java

--- task3-1.java
+++ task3-1.java
@@ -1,25 +1,31 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-package com.company;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
-  public Main() {
-  }
+public class PrimeFactorization {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число для разложения на простые множители: ");
+        int number = scanner.nextInt();
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println(removeDuplicates(scanner.nextLine().split("")));
-  }
+        ArrayList<Integer> factors = primeFactors(number);
+        System.out.println("Простые множители числа " + number + ": " + factors);
+    }
 
-  public static List<Integer> removeDuplicates(String[] input) {
-    int[] uniqueArray = Arrays.stream(input).mapToInt(Integer::parseInt).distinct().toArray();
-    return Arrays.stream(uniqueArray).boxed().toList();
-  }
+    public static ArrayList<Integer> primeFactors(int n) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        while (n % 2 == 0) {
+            factors.add(2);
+            n /= 2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        if (n > 2) {
+            factors.add(n);
+        }
+        return factors;
+    }
 }
